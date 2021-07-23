621bf20712431368f8035754593917b1326a04e1
Joe Frizzell <matchajoejoe@gmail.com>
2021-07-23 01:44:30 -0400

    fleshing out cricketHit and cricketMiss

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function checkHash(){
 function hideAll(){
   var gametypes = document.getElementsByClassName('game-type');
   for (gtindex = 0; gtindex < gametypes.length; gtindex++) {
-    gametypes[gtindex].style.display = "none";
+    gametypes[gtindex].style.display = 'none';
   }
   var gameselect = document.getElementById('game-select');
   gameselect.style.display = '';
@@ -31,15 +31,29 @@ function showCricket(){
 
 function newCricket() {
   var newCricketDiv = document.getElementById('new-cricket');
-  newCricketDiv.style.display = "flex";
+  newCricketDiv.style.display = 'flex';
   var cricketStartDiv = document.getElementById('cricket-start');
-  cricketStartDiv.style.display = "none";
+  cricketStartDiv.style.display = 'none';
   updatePlayers();
 }
 
 var currentPlayerCount = 0;
-var players = {};
-
+var players = [];
+var playerData = {
+  name: '',
+  points: 0,
+  rounds: [],
+  hitcounts:{
+    20: 0,
+    19: 0,
+    18: 0,
+    17: 0,
+    16: 0,
+    15: 0,
+    bull: 0
+  }
+}
+//
 function updatePlayers(){
   var playerCounter = document.getElementById('player-count');
   var playerTemplate = document.getElementById('player-template');
@@ -48,12 +62,17 @@ function updatePlayers(){
   var playerCountChange = newPlayerCount - currentPlayerCount;
   if(newPlayerCount > currentPlayerCount){
     for(i = 0; i < playerCountChange; i++){
-      newPlayerDiv = playerTemplate.cloneNode(true);
+      playersIndex = currentPlayerCount + i
       currentPlayerIndex = currentPlayerCount + 1 + i;
-      newPlayerDiv.id = "player-" + currentPlayerIndex;
-      newPlayerDiv.style.display = "block";
+      newPlayerData = JSON.parse(JSON.stringify(playerData));
+      newPlayerData.name = 'Player ' + currentPlayerIndex
+      window.players.push(newPlayerData);
+      newPlayerDiv = playerTemplate.cloneNode(true);
+      newPlayerDiv.id = 'player-' + currentPlayerIndex;
+      newPlayerDiv.setAttribute('players-index', playersIndex);
+      newPlayerDiv.style.display = 'block';
       nameLabel = newPlayerDiv.getElementsByTagName('label')[0];
-      nameLabel.innerHTML = "Player " + currentPlayerIndex;
+      nameLabel.innerHTML = 'Player ' + currentPlayerIndex;
       playerHolder.appendChild(newPlayerDiv);
     }
   } else if (newPlayerCount < currentPlayerCount){
@@ -63,7 +82,104 @@ function updatePlayers(){
   }
   currentPlayerCount = newPlayerCount;
 }
-
+// adding input to update player name
+function addNameInput(element){
+  nameHolder = element.parentNode;
+  element.style.display = 'none';
+  var nameInput = document.createElement('input');
+  nameInput.classList.add('name-input');
+  nameInput.addEventListener('keydown', function (e) {
+    if (e.code === 'Enter') {
+        updateName(nameInput);
+    }
+  });
+  var doneButton = document.createElement('button');
+  doneButton.innerHTML = '✓';
+  doneButton.classList.add('name-button')
+  doneButton.setAttribute('onclick','updateName(this)');
+  nameHolder.appendChild(nameInput);
+  nameHolder.appendChild(doneButton);
+}
+// updating name and removing input
 function updateName(element){
+  nameHolder = element.parentNode;
+  nameInput = nameHolder.getElementsByTagName('input')[0];
+  nameLabel = nameHolder.getElementsByTagName('label')[0];
+  nameButton = nameHolder.getElementsByTagName('button')[0];
+  newName = nameInput.value;
+  if(newName.length > 0){
+    nameLabel.innerHTML = newName;
+    playerHolder = nameHolder.parentNode;
+    playersIndex = playerHolder.getAttribute('players-index');
+    window.players[playersIndex].name = newName;
+  }
+  nameLabel.style.display = '';
+  nameHolder.removeChild(nameButton);
+  nameHolder.removeChild(nameInput);
+}
 
+function cricketHit(element, target, count){
+  rowHolder = element.parentNode;
+  targetDisplay = rowHolder.getElementsByTagName('input')[0];
+  playerHolder = rowHolder.parentNode.parentNode;
+  dartsCounter = playerHolder.getElementsByClassName('darts')[0];
+  roundCounter = playerHolder.getElementsByClassName('round')[0];
+  doneButton = playerHolder.getElementsByClassName('done-button')[0];
+  playersIndex = playerHolder.getAttribute('players-index');
+  currentPlayer = window.players[playersIndex];
+  currentRound = currentPlayer.rounds.length;
+  if(currentRound == 0){
+    currentPlayer.rounds.push([]);
+    currentRound = currentPlayer.rounds.length;
+  }
+  currentRoundIndex = currentRound - 1;
+  currentRoundArray = currentPlayer.rounds[currentRoundIndex];
+  currentDart = currentRoundArray.length;
+  if(currentDart == 3){
+    currentPlayer.rounds.push([]);
+    currentRound = currentPlayer.rounds.length;
+    currentRoundIndex = currentRound - 1;
+    currentRoundArray = currentPlayer.rounds[currentRoundIndex];
+    currentDart = currentRoundArray.length;
+  }
+  dartsLeft = 3 - (currentDart + 1);
+  doneButton.innerHTML = 'Done';
+  dartsCounter.innerHTML = dartsLeft.toString();
+  roundCounter.innerHTML = currentRound.toString();
+  currentRoundArray.push(target.toString() + "_" + count.toString());
+  currentPlayer.hitcounts[target] += count;
+  targetHitCount = currentPlayer.hitcounts[target];
+  if(targetHitCount == 1){
+    targetDisplay.value = '\\';
+  } else if (targetHitCount == 2){
+    targetDisplay.value = 'X';
+  } else {
+    targetDisplay.value = '0';
+  }
+}
+
+function cricketMiss(element){
+  rowHolder = element.parentNode;
+  targetDisplay = rowHolder.getElementsByTagName('input')[0];
+  playerHolder = rowHolder.parentNode.parentNode;
+  dartsCounter = playerHolder.getElementsByClassName('darts')[0];
+  roundCounter = playerHolder.getElementsByClassName('round')[0];
+  playersIndex = playerHolder.getAttribute('players-index');
+  currentPlayer = window.players[playersIndex];
+  currentRound = currentPlayer.rounds.length;
+  if(currentRound == 0){
+    currentPlayer.rounds.push([]);
+    currentRound = currentPlayer.rounds.length;
+  }
+  currentRoundIndex = currentRound - 1;
+  currentRoundArray = currentPlayer.rounds[currentRoundIndex];
+  currentDart = currentRoundArray.length;
+  dartsLeft = 3 - (currentDart + 1);
+  for(i = 0; i <= dartsLeft; i++){
+    currentRoundArray.push("miss");
+  }
+  dartsCounter.innerHTML = 0;
+  roundCounter.innerHTML = currentRound + 1;
+  element.innerHTML = "Miss";
+  console.log(players);
 }
