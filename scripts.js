def45302da1458102e7f05e4a8827189104a6126
Joe Frizzell <matchajoejoe@gmail.com>
2021-07-24 14:47:33 -0400

    fleshing out cricket points

--- scripts.js
+++ scripts.js
@@ -17,6 +17,9 @@ function hideAll(){
   }
   var gameselect = document.getElementById('game-select');
   gameselect.style.display = '';
+  playerHolder = document.getElementById('player-holder');
+  playerHolder.innerHTML = '';
+  window.players = [];
 }
 
 function hideSelect(){
@@ -27,6 +30,10 @@ function hideSelect(){
 function showCricket(){
   hideSelect();
   var cricketdiv = document.getElementById('cricket').style.display = '';
+  var newCricketDiv = document.getElementById('new-cricket');
+  newCricketDiv.style.display = 'none';
+  var cricketStartDiv = document.getElementById('cricket-start');
+  cricketStartDiv.style.display = 'flex';
 }
 
 function newCricket() {
@@ -51,9 +58,11 @@ var playerData = {
   },
   name: '',
   points: 0,
-  rounds: [[]],
-  states: []
+  rounds: [[]]
 }
+var states = [];
+var lowestRound = 0;
+cricketTargetList = ['20', '19', '18', '17', '16', '15', 'bull'];
 // add cricket player template to player holder
 function addCricketPlayer(){
   playerTemplate = document.getElementById('cricket-template');
@@ -70,6 +79,8 @@ function addCricketPlayer(){
   nameLabel = newPlayerDiv.getElementsByTagName('label')[0];
   nameLabel.innerHTML = 'Player ' + playerNumber;
   playerHolder.appendChild(newPlayerDiv);
+  checkRoundEnd();
+  checkAllTargets();
 }
 // remove player from player template and players.
 function removePlayer(element){
@@ -133,11 +144,11 @@ function updateName(element){
 }
 // updating display for a hit in cricket
 function cricketHit(element, target, count){
+  addState();
   rowHolder = element.parentNode;
   targetDisplay = rowHolder.getElementsByTagName('input')[0];
   playerHolder = rowHolder.parentNode.parentNode;
   playersIndex = playerHolder.getAttribute('players-index');
-  addPlayerState(playersIndex);
   dartsCounter = playerHolder.getElementsByClassName('darts')[0];
   roundCounter = playerHolder.getElementsByClassName('round')[0];
   doneButton = playerHolder.getElementsByClassName('done-button')[0];
@@ -171,13 +182,18 @@ function cricketHit(element, target, count){
     targetDisplay.value = 'X';
   } else {
     targetDisplay.value = '0';
-    if(targetHitCount > 3){
-      if(oldHitCounts < 3){
-        newCount = count - (3 - oldHitCounts);
-      } else {
-        newCount = count;
+    targetClosed = checkTargetClosed(target);
+    if(!targetClosed){
+      if(targetHitCount > 3){
+        if(oldHitCounts < 3){
+          newCount = count - (3 - oldHitCounts);
+        } else {
+          newCount = count;
+        }
+        calculatePoints(playersIndex, target, newCount);
       }
-      calculatePoints(playersIndex, target, newCount);
+    } else {
+      currentPlayer.hitcounts[target] = 3;
     }
   }
   if(dartsLeft == 0){
@@ -186,73 +202,172 @@ function cricketHit(element, target, count){
 }
 
 function calculatePoints(playersIndex, target, newCount) {
-  console.log("points not implemented yet...");
-}
-// add state to player
-function addPlayerState(playersIndex){
   thisPlayer = window.players[playersIndex];
-  newPlayerData = JSON.parse(JSON.stringify(thisPlayer));
-  newPlayerData.states = [];
-  thisPlayer.states.push(newPlayerData);
-  thisPlayerDiv = document.getElementById('player-' + (parseInt(playersIndex) + 1));
-  thisUndoButton = thisPlayerDiv.getElementsByClassName('undo-button')[0];
-  thisUndoButton.disabled = false;
-}
-function undoButton(element){
-  rowHolder = element.parentNode;
-  playerHolder = rowHolder.parentNode.parentNode;
-  playersIndex = playerHolder.getAttribute('players-index');
-  playerStatesLength = window.players[playersIndex].states.length;
-  if(playerStatesLength > 0){
-    loadPreviousState(playersIndex);
-    if(playerStatesLength == 1){
-      element.disabled = true;
+    otherTargetsOpen = true;
+    for(p = 0; p < window.players.length; p++){
+      if(p != playersIndex){
+        otherPlayer = window.players[p];
+        if(otherPlayer.hitcounts[target] > 2){
+          otherTargetsOpen = false;
+        }
+      }
     }
+    if(target == 'bull'){
+      pointsValue = 25;
+    } else {
+      pointsValue = parseInt(target);
+    }
+    if(otherTargetsOpen){
+      thisPlayer.points += pointsValue * newCount;
+    } else {
+      for(p = 0; p < window.players.length; p++){
+        if(p != playersIndex){
+          otherPlayer = window.players[p];
+          if(otherPlayer.hitcounts[target] < 3){
+            otherPlayer.points -= pointsValue * newCount;
+          }
+        }
+      }
+    }
+  displayPoints();
+}
+
+function displayPoints(){
+  for(dp = 0; dp < window.players.length; dp++){
+    pointsPlayer = window.players[dp];
+    pointsPlayerDiv = document.getElementById('player-' + (dp + 1));
+    scoreSpan = pointsPlayerDiv.getElementsByClassName('score')[0];
+    scoreSpan.innerHTML = pointsPlayer.points;
+  }
+}
+
+function checkAllTargets(){
+  for(t = 0; t < window.cricketTargetList.length; t++){
+    thisTarget = window.cricketTargetList[t];
+    checkTargetClosed(thisTarget);
+  }
+}
+
+function checkTargetClosed(target){
+  targetClosed = true;
+  for(i = 0; i < window.players.length; i++){
+    thisPlayer = window.players[i];
+    if(thisPlayer.hitcounts[target] < 3){
+      targetClosed = false;
+    }
+  }
+  if(targetClosed){
+    disableTargetButtons(target);
   } else {
-    element.disabled = true;
+    enableTargetButtons(target);
   }
+  return targetClosed;
 }
-function loadPreviousState(playersIndex){
-  thisPlayer = window.players[playersIndex];
-  playerStatesLength = thisPlayer.states.length;
-  previousState = thisPlayer.states[playerStatesLength-1];
-  thisPlayer.hitcounts = previousState.hitcounts;
-  thisPlayer.name = previousState.name;
-  thisPlayer.points = previousState.points;
-  thisPlayer.rounds = previousState.rounds;
-  thisPlayer.states.pop();
-  displayPlayerData(playersIndex);
-}
-function displayPlayerData(playersIndex){
-  thisPlayer = window.players[playersIndex];
-  if(!thisPlayer){
-    console.error("player " + playersIndex + " does not exits...");
-  }
-  thisPlayerDiv = document.getElementById('player-' + (parseInt(playersIndex) + 1));
-  if(!thisPlayerDiv){
-    thisPlayerDiv = createPlayerDivFromIndex(playersIndex);
-  }
-  nameLabel = thisPlayerDiv.getElementsByTagName('label')[0];
-  nameLabel = thisPlayer.name;
-  currentRound = thisPlayer.rounds.length;
-  if(currentRound == 0){
-    thisPlayer.rounds.push([]);
-    currentRound = thisPlayer.rounds.length;
-  }
-  roundCounter = thisPlayerDiv.getElementsByClassName('round')[0];
-  roundCounter.innerHTML = currentRound;
-  currentRoundIndex = currentRound - 1;
-  currentRoundArray = thisPlayer.rounds[currentRoundIndex];
-  currentDart = currentRoundArray.length;
-  dartsLeft = 3 - (currentDart);
-  dartsCounter = thisPlayerDiv.getElementsByClassName('darts')[0];
-  dartsCounter.innerHTML = dartsLeft;
-  enablePlayerButtons(thisPlayerDiv);
-  hitCounters = thisPlayerDiv.getElementsByClassName('hitcounter');
+
+function disableTargetButtons(target){
+  hitCounters = document.getElementsByClassName('hitcounter');
   for(i = 0; i < hitCounters.length; i++){
     thisHitCounter = hitCounters[i];
-    thisTarget = thisHitCounter.getAttribute("hitcounter");
-    targetHitCount = thisPlayer.hitcounts[thisTarget];
+    thisTarget = thisHitCounter.getAttribute('hitcounter');
+    if(thisTarget == target){
+      rowHolder = thisHitCounter.parentNode;
+      targetButtons = rowHolder.getElementsByTagName('button');
+      for(b = 0; b < targetButtons.length; b++){
+        targetButtons[b].disabled = true;
+      }
+    }
+  }
+}
+function enableTargetButtons(target){
+  let hitCounters = document.getElementsByClassName('hitcounter');
+  for(let i = 0; i < hitCounters.length; i++){
+    let thisHitCounter = hitCounters[i];
+    let thisTarget = thisHitCounter.getAttribute('hitcounter');
+    if(thisTarget == target){
+      rowHolder = thisHitCounter.parentNode;
+      let targetPlayerDiv = rowHolder.parentNode.parentNode;
+      let targetPlayersIndex = targetPlayerDiv.getAttribute('players-index');
+      if(targetPlayersIndex != null){
+        let targetPlayer = window.players[targetPlayersIndex];
+        let targetPlayerRound = targetPlayer.rounds.length;
+        let dartsCounter = targetPlayerDiv.getElementsByClassName('darts')[0];
+        if(dartsCounter.innerHTML != '0' && targetPlayerRound == window.lowestRound){
+          let targetButtons = rowHolder.getElementsByTagName('button');
+          for(b = 0; b < targetButtons.length; b++){
+            targetButtons[b].disabled = false;
+          }
+        }
+      }
+    }
+  }
+}
+// add state to player
+function addState(){
+  newPlayersData = JSON.parse(JSON.stringify(window.players));
+  window.states.push(newPlayersData);
+  let undoButton = document.getElementById('undo-button');
+  undoButton.disabled = false;
+}
+// undo last player state
+function undoButton(){
+  let undoButton = document.getElementById('undo-button');
+  statesLength = window.states.length;
+  if(statesLength > 0){
+    loadPreviousState();
+    if(statesLength == 1){
+      undoButton.disabled = true;
+    }
+  } else {
+    undoButton.disabled = true;
+  }
+}
+function loadPreviousState(){
+  statesLength = window.states.length;
+  previousState = window.states[statesLength - 1];
+  for(let i = 0; i < window.players.length; i++){
+    if(i < previousState.length){
+      let thisPlayer = window.players[i];
+      thisPlayer.hitcounts = previousState[i].hitcounts;
+      thisPlayer.name = previousState[i].name;
+      thisPlayer.points = previousState[i].points;
+      thisPlayer.rounds = previousState[i].rounds;
+      displayPlayerData(i);
+    }
+  }
+  window.states.pop();
+}
+function displayPlayerData(playersIndex){
+  displayPlayer = window.players[playersIndex];
+  if(!displayPlayer){
+    console.error("player " + playersIndex + " does not exits...");
+  }
+  displayPlayerDiv = document.getElementById('player-' + (parseInt(playersIndex) + 1));
+  if(!displayPlayerDiv){
+    displayPlayerDiv = createPlayerDivFromIndex(playersIndex);
+  }
+  nameLabel = displayPlayerDiv.getElementsByTagName('label')[0];
+  nameLabel = displayPlayer.name;
+  currentRound = displayPlayer.rounds.length;
+  if(currentRound == 0){
+    displayPlayer.rounds.push([]);
+    currentRound = displayPlayer.rounds.length;
+  }
+  roundCounter = displayPlayerDiv.getElementsByClassName('round')[0];
+  roundCounter.innerHTML = currentRound;
+  currentRoundIndex = currentRound - 1;
+  currentRoundArray = displayPlayer.rounds[currentRoundIndex];
+  currentDart = currentRoundArray.length;
+  dartsLeft = 3 - (currentDart);
+  dartsCounter = displayPlayerDiv.getElementsByClassName('darts')[0];
+  dartsCounter.innerHTML = dartsLeft;
+  scoreCounter = displayPlayerDiv.getElementsByClassName('score')[0];
+  scoreCounter.innerHTML = displayPlayer.points;
+  enablePlayerButtons(displayPlayerDiv);
+  hitCounters = displayPlayerDiv.getElementsByClassName('hitcounter');
+  for(i = 0; i < hitCounters.length; i++){
+    thisHitCounter = hitCounters[i];
+    thisTarget = thisHitCounter.getAttribute('hitcounter');
+    targetHitCount = displayPlayer.hitcounts[thisTarget];
     if(targetHitCount == 0){
       thisHitCounter.value = '';
     } else if(targetHitCount == 1){
@@ -263,9 +378,10 @@ function displayPlayerData(playersIndex){
       thisHitCounter.value = '0';
     }
   }
+  checkAllTargets();
 }
 function createPlayerDivFromIndex(playersIndex){
-  thisPlayer = window.players[playersIndex];
+  createPlayer = window.players[playersIndex];
   playerTemplate = document.getElementById('cricket-template');
   playerHolder = document.getElementById('player-holder');
   playerNumber = playersIndex + 1;
@@ -274,19 +390,19 @@ function createPlayerDivFromIndex(playersIndex){
   newPlayerDiv.setAttribute('players-index', playersIndex);
   newPlayerDiv.style.display = 'block';
   nameLabel = newPlayerDiv.getElementsByTagName('label')[0];
-  nameLabel.innerHTML = thisPlayer.name;
+  nameLabel.innerHTML = createPlayer.name;
   playerHolder.appendChild(newPlayerDiv);
   return newPlayerDiv;
 }
 // updating display for a miss in cricket
 function cricketMiss(element){
+  addState();
   rowHolder = element.parentNode;
   targetDisplay = rowHolder.getElementsByTagName('input')[0];
   playerHolder = rowHolder.parentNode.parentNode;
   dartsCounter = playerHolder.getElementsByClassName('darts')[0];
   roundCounter = playerHolder.getElementsByClassName('round')[0];
   playersIndex = playerHolder.getAttribute('players-index');
-  addPlayerState(playersIndex);
   currentPlayer = window.players[playersIndex];
   currentRound = currentPlayer.rounds.length;
   if(currentRound == 0){
@@ -319,24 +435,14 @@ function disablePlayerButtons(element, checkEnd = true){
 // check if round is over
 function checkRoundEnd(){
   // check if everyone is on the same round
-  roundsEqual = true;
-  lowestRound = window.players[0].rounds.length;
-  for(i = 0; i < window.players.length; i++){
-    currentPlayerRound = window.players[i].rounds.length;
-    if(currentPlayerRound < lowestRound){
-      roundsEqual = false;
-      lowestRound = currentPlayerRound;
-    }
-  }
+  roundsEqual = checkPlayerRounds();
   if(roundsEqual){
-    //TODO: this is broken, need to fix it
-
     // everyone is on the same round, check if round complete
     roundComplete = true;
     for(i = 0; i < window.players.length; i++){
-      thisPlayer = window.players[i];
-      currentRound = thisPlayer.rounds.length - 1;
-      if(thisPlayer.rounds[currentRound].length < 3){
+      checkPlayer = window.players[i];
+      currentRound = checkPlayer.rounds.length - 1;
+      if(checkPlayer.rounds[currentRound].length < 3){
         roundComplete = false;
       }
     }
@@ -347,14 +453,37 @@ function checkRoundEnd(){
     // give players time to catch up
     for(j = 0; j < window.players.length; j++){
       currentPlayerRound = window.players[j].rounds.length;
-      thisPlayerDiv = document.getElementById('player-' + (j + 1));
-      if(currentPlayerRound > lowestRound){
-        disablePlayerButtons(thisPlayerDiv, false);
-      } else {
-        enablePlayerButtons(thisPlayerDiv);
+      checkPlayerDiv = document.getElementById('player-' + (j + 1));
+      if(currentPlayerRound == window.lowestRound){
+        startCricketPlayerRound(j);
       }
     }
   }
+}
+function checkPlayerRounds(){
+  // check if everyone is on the same round
+  roundsEqual = true;
+  window.lowestRound = window.players[0].rounds.length;
+  for(i = 0; i < window.players.length; i++){
+    currentPlayerRound = window.players[i].rounds.length;
+    if(currentPlayerRound < window.lowestRound){
+      roundsEqual = false;
+      window.lowestRound = currentPlayerRound;
+    }
+  }
+  if(!roundsEqual){
+    // give players time to catch up
+    for(j = 0; j < window.players.length; j++){
+      currentPlayerRound = window.players[j].rounds.length;
+      checkPlayerDiv = document.getElementById('player-' + (j + 1));
+      if(currentPlayerRound > window.lowestRound){
+        disablePlayerButtons(checkPlayerDiv, false);
+      } else {
+        enablePlayerButtons(checkPlayerDiv);
+      }
+    }
+  }
+  return roundsEqual;
 }
 // enable buttons for players that need to catch up
 function enablePlayerButtons(element){
@@ -363,24 +492,34 @@ function enablePlayerButtons(element){
     thisButton = playerButtons[i];
     thisButton.disabled = false;
   }
+  checkAllTargets();
 }
 
 function startNewCricketRound(){
   playerHolder = document.getElementById('player-holder');
   for(i = 0; i < window.players.length; i++){
-    thisPlayer = window.players[i];
-    thisPlayer.rounds.push([]);
-    thisPlayerDiv = document.getElementById('player-' + (i + 1));
-    roundCounter = thisPlayerDiv.getElementsByClassName('round')[0];
-    roundCounter.innerHTML = thisPlayer.rounds.length.toString();
-    dartsCounter = thisPlayerDiv.getElementsByClassName('darts')[0];
-    dartsCounter.innerHTML = '3';
-    doneButton = thisPlayerDiv.getElementsByClassName('done-button')[0];
-    doneButton.innerHTML = 'Miss';
+    startCricketPlayerRound(i);
   }
   playerButtons = playerHolder.getElementsByTagName('button');
   for(i = 0; i < playerButtons.length; i++){
     thisButton = playerButtons[i];
     thisButton.disabled = false;
+  }
+  checkAllTargets();
+}
+function startCricketPlayerRound(playersIndex){
+  let roundPlayer = window.players[playersIndex];
+  let currentRound = roundPlayer.rounds.length;
+  let roundIndex = currentRound - 1;
+  let roundPlayerDarts = roundPlayer.rounds[roundIndex].length;
+  if(roundPlayerDarts == 3){
+    roundPlayer.rounds.push([]);
+    let roundPlayerDiv = document.getElementById('player-' + (playersIndex + 1));
+    let roundCounter = roundPlayerDiv.getElementsByClassName('round')[0];
+    roundCounter.innerHTML = roundPlayer.rounds.length.toString();
+    let dartsCounter = roundPlayerDiv.getElementsByClassName('darts')[0];
+    dartsCounter.innerHTML = '3';
+    let doneButton = roundPlayerDiv.getElementsByClassName('done-button')[0];
+    doneButton.innerHTML = 'Miss';
   }
 }
